300dfd3c38c95d38e2adab99d2edc50b891cc716
Abdulrehman1793 <abdulrehman1793@gmail.com>
2023-08-04 16:32:04 +0530

    Added assembly and other cost entity

--- manufacturing-manager-api/src/main/resources/db/migration/V0.0__init_schema.sql
+++ manufacturing-manager-api/src/main/resources/db/migration/V0.0__init_schema.sql
@@ -6,22 +6,36 @@ CREATE TABLE unit_of_measure
     name varchar(50) not null unique
 );
 
+drop table if exists purchase_unit;
 CREATE TABLE purchase_unit
 (
-    id   varchar(10)  not null primary key,
+    id   varchar(10) not null primary key,
     name varchar(50) not null unique
 );
 
+drop table if exists raw_goods_type;
 CREATE TABLE raw_goods_type
 (
-    id   varchar(10)  not null primary key,
-    name varchar(50) not null unique,
-    description varchar(250)  null
+    id          varchar(10)  not null primary key,
+    name        varchar(50)  not null unique,
+    description varchar(250) null
 );
 
+drop table if exists finished_goods_type;
 CREATE TABLE finished_goods_type
 (
-    id   varchar(10)  not null primary key,
-    name varchar(50) not null unique,
-    description varchar(250)  null
+    id          varchar(10)  not null primary key,
+    name        varchar(50)  not null unique,
+    description varchar(250) null
+);
+
+drop table if exists cost;
+CREATE TABLE cost
+(
+    id          serial primary key,
+    name        varchar(50)  not null unique,
+    description varchar(500) null,
+    cost_type   varchar(50)  null,
+    cost_unit   varchar(50)  null,
+    amount      double precision
 );
