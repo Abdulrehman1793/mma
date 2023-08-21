49897c88165c55d021fca8b919f5cbca137f8daf
Abdulrehman1793 <abdulrehman1793@gmail.com>
2023-08-21 20:08:57 +0530

    Added angular table component for common usage

--- ng-manufacturing-manager/src/app/app.module.ts
+++ ng-manufacturing-manager/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { CoreModule } from './core/core.module';
+import { TableComponent } from './common/table/table.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +15,7 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
+    TableComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ ng-manufacturing-manager/src/app/common/table/table.component.ts
@@ -0,0 +1,108 @@
+import {
+  AfterViewInit,
+  Component,
+  ViewChild,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+
+import {
+  MatPaginator,
+  MatPaginatorModule,
+  PageEvent,
+} from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {
+  EMPTY,
+  Observable,
+  catchError,
+  merge,
+  of,
+  startWith,
+  switchMap,
+} from 'rxjs';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
+  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
+  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
+  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
+  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
+  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
+  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
+  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
+  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
+  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
+];
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss'],
+  standalone: true,
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule],
+})
+export class TableComponent implements AfterViewInit {
+  @Input() columns: string[] = [];
+  @Input('rows') rows$: Observable<any[]> = EMPTY;
+
+  @Input() totalRows: number = 0;
+
+  @Output() onChange: EventEmitter<any> = new EventEmitter<any>();
+
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+
+    // If the user changes the sort order, reset back to the first page.
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+
+    merge(this.sort.sortChange, this.paginator.page)
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          // Calculate the pageIndex and pageSize based on MatPaginator's properties
+          const pageIndex = this.paginator.pageIndex;
+          const pageSize = this.paginator.pageSize;
+
+          const sortField = this.sort.active;
+          const sortDirection = this.sort.direction;
+
+          console.log("working");
+
+          this.onChange.emit({ pageIndex, pageSize, sortField, sortDirection });
+
+          return of([]);
+        }),
+        catchError(() => {
+          return of([]);
+        })
+      )
+      .subscribe();
+  }
+}
